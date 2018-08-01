11c4c411f8a14ff84259168ecbb163d3d8c7e0d3
Unknown <andrew_khang_le@hotmail.com>
2018-08-01 17:38:00 -0500

    Add color code to the start and finish

--- Assets/Pathfinder.cs
+++ Assets/Pathfinder.cs
@@ -4,11 +4,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Pathfinder : MonoBehaviour {
+    [SerializeField] Waypoint startWaypoint, endWaypoint;
     Dictionary<Vector2Int, Waypoint> grid = new Dictionary<Vector2Int, Waypoint>(); 
-	// Use this for initialization
+	
+    // Use this for initialization
 	void Start () {
         LoadBlocks();
+        ColorStartAndEnd();
 	}
+
+    private void ColorStartAndEnd(){
+        var waypoints = FindObjectsOfType<Waypoint>();
+        foreach (Waypoint waypoint in waypoints){
+            waypoint.SetTopColor(Color.gray);//change all top to grey
+        }
+
+        startWaypoint.SetTopColor(Color.green);//set start as green
+        endWaypoint.SetTopColor(Color.red);//set end as red
+    }
 
     private void LoadBlocks(){
         var waypoints = FindObjectsOfType<Waypoint>();
