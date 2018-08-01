36f503b61ad282c13c18e79fb70b96773ed84689
Unknown <andrew_khang_le@hotmail.com>
2018-08-01 12:49:25 -0500

    refactor code using encapsulation

--- Assets/CubeEditor.cs
+++ Assets/CubeEditor.cs
@@ -4,19 +4,36 @@ using UnityEngine;
 
 [ExecuteInEditMode]
 [SelectionBase]
+[RequireComponent(typeof(Waypoint))]
+
 public class CubeEditor : MonoBehaviour {
-    [SerializeField][Range(1f,20f)] float gridSize = 10f;
-    TextMesh textMesh;
+    Vector3 gridPos;
+    Waypoint waypoint;
 
-    void Update(){
-        Vector3 snapPos;
+    private void Awake(){
+        waypoint = GetComponent<Waypoint>();
+    }
 
-        snapPos.x = Mathf.RoundToInt(transform.position.x/gridSize) * gridSize;
-        snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
-        transform.position = new Vector3(snapPos.x,0f,snapPos.z);
+    void Update()
+    {
+        SnapToGrid();
+        UpdateLabel();
+    }
 
-        textMesh = GetComponentInChildren<TextMesh>();
-        string labelText = snapPos.x/gridSize + "," + snapPos.z/gridSize;
+    private void SnapToGrid()
+    {
+        int gridSize = waypoint.GetGridSize();//get from encapsulated waypoint getter
+        transform.position = new Vector3(
+            waypoint.GetGridPos().x,
+            0f,
+            waypoint.GetGridPos().y
+        );
+    }
+    private void UpdateLabel(){
+        int gridSize = waypoint.GetGridSize();
+        TextMesh textMesh = GetComponentInChildren<TextMesh>();
+        string labelText = waypoint.GetGridPos().x/ gridSize + "," + 
+                           waypoint.GetGridPos().y / gridSize;
         textMesh.text = labelText;
         gameObject.name = labelText;
     }
--- Assets/Waypoint.cs
+++ Assets/Waypoint.cs
@@ -2,15 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Waypoint : MonoBehaviour {
+//encapsulation
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+public class Waypoint : MonoBehaviour {
+    Vector2Int gridPos;
+    const int gridSize = 10;
+
+    public int GetGridSize() {
+        return gridSize;
+    }
+
+    public Vector2 GetGridPos() {
+        return new Vector2Int (
+            Mathf.RoundToInt(transform.position.x / gridSize) * gridSize,
+            Mathf.RoundToInt(transform.position.z / gridSize) * gridSize
+        );
+    }
 }
