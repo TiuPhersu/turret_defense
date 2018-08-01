fdfaa46ff45f83cfc355e979b8b743dd87a93726
Unknown <andrew_khang_le@hotmail.com>
2018-08-01 08:32:59 -0500

    Have the enemies follow a path

--- Assets/EnemyMovement.cs
+++ Assets/EnemyMovement.cs
@@ -3,15 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour {
-    [SerializeField] List<Block> path;
+    [SerializeField] List<Waypoint> path;
 	// Use this for initialization
-	void Start () {
-        foreach(Block waypoint in path){
-            print(waypoint.name);
-        }
-
+	void Start (){
+        StartCoroutine(FollowPath());
     }
-	
+
+    IEnumerator FollowPath(){
+        print("Starting patrol...");
+        foreach (Waypoint waypoint in path){
+            //print(waypoint.name);
+            transform.position = waypoint.transform.position;// enemy moves
+            print("Visiting blocks " + waypoint.name);
+            yield return new WaitForSeconds(1f);
+        }
+        print("Ending patrol...");
+    }
+    
 	// Update is called once per frame
 	void Update () {
 		
