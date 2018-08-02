dbba67e2bbc1b528e8c684fa45d16c263c9d6131
Unknown <andrew_khang_le@hotmail.com>
2018-08-02 12:11:51 -0500

    Check the neighbors of the current location

--- Assets/Pathfinder.cs
+++ Assets/Pathfinder.cs
@@ -5,13 +5,31 @@ using UnityEngine;
 
 public class Pathfinder : MonoBehaviour {
     [SerializeField] Waypoint startWaypoint, endWaypoint;
-    Dictionary<Vector2Int, Waypoint> grid = new Dictionary<Vector2Int, Waypoint>(); 
-	
+    Dictionary<Vector2Int, Waypoint> grid = new Dictionary<Vector2Int, Waypoint>();
+    Vector2Int[] directions = { Vector2Int.up,
+                                Vector2Int.right,
+                                Vector2Int.down,
+                                Vector2Int.left};
+
     // Use this for initialization
 	void Start () {
         LoadBlocks();
         ColorStartAndEnd();
+        ExploreNeighbours();
 	}
+
+    private void ExploreNeighbours()
+    {
+        foreach (Vector2Int direction in directions) {
+            Vector2Int explorationCoordinates = direction + startWaypoint.GetGridPos(); 
+            print("Exploring " + explorationCoordinates);
+            try{
+                grid[explorationCoordinates].SetTopColor(Color.blue);
+            }
+            catch { //do nothing
+            }
+        }
+    }
 
     private void ColorStartAndEnd(){
         var waypoints = FindObjectsOfType<Waypoint>();
