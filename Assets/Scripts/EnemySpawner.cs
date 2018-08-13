621ee9a5e65f7c4508c8106ab4274ba8c5a8ce32
Unknown <andrew_khang_le@hotmail.com>
2018-08-13 14:09:10 -0500

    Implement target system

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+    [SerializeField] float secondsBetweenSpawns = 10f;
+    [SerializeField] EnemyMovement enemyPrefab;
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(Spawn());
+    }
+
+    IEnumerator Spawn(){
+        while (true){
+            Instantiate(enemyPrefab, transform.position, Quaternion.identity);
+            //print("Spawning");
+            yield return new WaitForSeconds(secondsBetweenSpawns);
+        }
+    }
+}
--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -1,15 +1,19 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Tower : MonoBehaviour {
+    //Parameters of each tower
     [SerializeField] Transform objectToPan;
-    [SerializeField] Transform targetEnemy;
     [SerializeField] float attackRange = 10f;
     [SerializeField] ParticleSystem projectileParticle;
-	
+
+    //State of each tower
+    Transform targetEnemy;
 	// Update is called once per frame
 	void Update () {
+        SetTargetEnemy();
         if (targetEnemy){
             objectToPan.LookAt(targetEnemy);// look at enemy;
             FireAtEnemy();//determin the distance
@@ -17,6 +21,32 @@ public class Tower : MonoBehaviour {
             Shoot(false);
         }
 	}
+
+    private void SetTargetEnemy(){
+        // Get the collection of things
+        var sceneEnemies = FindObjectsOfType<EnemyDamage>();
+        if (sceneEnemies.Length == 0) { return; }
+        //Assume the first is the "winner"
+        Transform closetEnemy = sceneEnemies[0].transform;
+        // for each item in collection
+        foreach (EnemyDamage testEnemy in sceneEnemies) {
+            // update winner
+            closetEnemy = GetCloset(closetEnemy, testEnemy.transform);
+        }
+        // return the winner
+        targetEnemy = closetEnemy;
+    }
+
+    private Transform GetCloset(Transform transformA , Transform transformB){
+        var distToA = Vector3.Distance(transform.position, transformA.position);
+        var distToB = Vector3.Distance(transform.position, transformA.position);
+
+        if (distToA < distToB) {
+            return transformA;
+        }
+        return transformB;
+    }
+
     private void FireAtEnemy() {
         float distanceToEnemy = Vector3.Distance(targetEnemy.transform.position, gameObject.transform.position);
         if (distanceToEnemy <= attackRange) {
