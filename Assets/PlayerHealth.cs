8b38db86cdd2e343a3bc886ee08842fd5bfa6b3d
Unknown <andrew_khang_le@hotmail.com>
2018-08-20 12:39:31 -0500

    Add UI
    UI for the score tracker and health tracker

--- Assets/PlayerHealth.cs
+++ Assets/PlayerHealth.cs
@@ -1,14 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;//use the user interface text box
 
 public class PlayerHealth : MonoBehaviour {
 
     [SerializeField] int health = 10;
     [SerializeField] int healthDecrease = 1;
+    [SerializeField] Text healthText;
+
+    private void Start(){
+        healthText.text = health.ToString();
+    }
+
+    private void Update(){
+        healthText.text = health.ToString();
+    }
 
     private void OnTriggerEnter(Collider other){
         health = health - healthDecrease;
     }
+
+
 
 }
--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -1,19 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemySpawner : MonoBehaviour {
     [SerializeField] float secondsBetweenSpawns = 10f;
     [SerializeField] EnemyMovement enemyPrefab;
-	// Use this for initialization
-	void Start () {
+    [SerializeField] Text scoreText;
+    [SerializeField] Transform enemyParentTransform;
+
+    int score = 0;
+
+    // Use this for initialization
+    void Start () {
         StartCoroutine(Spawn());
     }
 
     IEnumerator Spawn(){
         while (true){
-            Instantiate(enemyPrefab, transform.position, Quaternion.identity);
+            var newEnemy = Instantiate(enemyPrefab, transform.position, Quaternion.identity);//spawn enemy
+            newEnemy.transform.parent = enemyParentTransform;//place enemy into the object enemies(more organized)
+
             //print("Spawning");
+            score++;
+            scoreText.text = score.ToString();
+
             yield return new WaitForSeconds(secondsBetweenSpawns);
         }
     }
